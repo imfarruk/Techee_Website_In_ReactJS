bb47f2ae6d2536be3ed93a5688b11e67a303f0c0
Md Farhan Haider <imfarruk@gmail.com>
2021-07-10 00:44:37 +0530

    Deploy teche website in reactjs with the support of bootstrap

---
+++ src/ReactWebsite/CardData.js
@@ -0,0 +1,35 @@
+import dev1 from "./images/contact.png";
+import dev2 from "./images/dev4.jpg";
+import dev3 from "./images/dev5.jpg";
+import dev4 from "./images/contact.png";
+import dev5 from "./images/dev4.jpg";
+import dev6 from "./images/dev5.jpg";
+
+const CardData = [
+  {
+    imgsrc: dev1,
+    title: "Web Development",
+  },
+  {
+    imgsrc: dev2,
+    title: "E Commerce",
+  },
+  {
+    imgsrc: dev3,
+    title: "Wordpress",
+  },
+  {
+    imgsrc: dev4,
+    title: "Cloud Services",
+  },
+  {
+    imgsrc: dev5,
+    title: "Machine Learning",
+  },
+  {
+    imgsrc: dev6,
+    title: "Artificial Intelligence",
+  },
+];
+
+export default CardData;
